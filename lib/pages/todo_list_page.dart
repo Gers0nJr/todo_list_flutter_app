23266c93cb79a4d09cc7df245d1865f6fea28f16
Gerson Batista <gersonkenedy@hotmail.com>
2024-08-04 11:11:31 -0300

    limpar message

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -129,6 +129,7 @@ class _TodoListPageState extends State<TodoListPage> {
       todos.remove(todo);
     });
 
+    ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       content: Text(
         'Tarefa ${todo.title} foi removida com sucesso!',
@@ -144,6 +145,7 @@ class _TodoListPageState extends State<TodoListPage> {
           });
         },
       ),
+      duration: const Duration(seconds: 5),
     ));
   }
 }
